086c1022dc182215a385965987c53d6bce923fdc
James Hershey <jameshershey34@gmail.com>
2024-02-01 16:03:47 -0500

    fibonacci partner practice. py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,17 @@
-def fibonacci(n):
-  pass
+def fibonacci(num):
+  if num <= 1:
+    return num
 
+  a = 0
+  b = 1
+  ph = 1
+  for num in range(1, num, +1):
+    ph = a + b
+    a = b
+    b = ph
+  return ph
+
+print(fibonacci(1))
+print(fibonacci(5))
+print(fibonacci(7))
 
